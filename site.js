4b9da7adb4062b6e92e43639938fa7f2098bc51a
dtumonasingh <dtumonasingh@gmail.com>
2022-03-25 00:54:24 +0530

    Added a basic carousal layout with left and right arrows

--- site.js
+++ site.js
@@ -1,1 +1,7 @@
+document.getElementById("left-arrow").onclick(   
+    console.log('left arrow clicked')
+)
 
+document.getElementById("right-arrow").onclick(   
+    console.log('right arrow clicked')
+)
