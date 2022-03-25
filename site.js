4c708970e52c7286cd9fe8fc90c31e3ade6f74f1
dtumonasingh <dtumonasingh@gmail.com>
2022-03-25 16:57:09 +0530

    feature: Added the logic to switch images

--- site.js
+++ site.js
@@ -1,7 +1,26 @@
-document.getElementById("left-arrow").onclick(   
-    console.log('left arrow clicked')
-)
+var count=1;
+document.getElementById('item1').style.zIndex = 1;
 
-document.getElementById("right-arrow").onclick(   
-    console.log('right arrow clicked')
-)
+document.getElementById('left-arrow').onclick = function() {
+    document.getElementById(`item${count}`).style.zIndex = 0;
+    count=count-1;
+    if(count==0)
+        count=5;
+
+    console.log(count)
+    document.getElementById(`item${(count)}`).style.zIndex = 1;
+    
+}
+
+document.getElementById('right-arrow').onclick = function() {
+    document.getElementById(`item${count}`).style.zIndex = 0;
+    count=count+1;
+    if(count==6)
+    count=1;
+
+    console.log(count)
+    document.getElementById(`item${(count)}`).style.zIndex = 1;
+   
+}
+
+
